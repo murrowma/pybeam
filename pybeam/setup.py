cbc09676d7115dae2b09e2192fa5454e457989c4
Matthew Murrow <68360403+MatthewMurrow@users.noreply.github.com>
2022-01-30 23:56:22 -0600

    Delete pybeam directory

--- pybeam/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-from Cython.Build import cythonize
-
-setup(name='pybeam',
-      version='0.1',
-      packages=find_packages(),
-      ext_modules = cythonize(["pybeam/default/functions_default.pyx"]),
-      zip_safe=False)
